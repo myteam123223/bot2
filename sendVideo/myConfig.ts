bbce47b6f33bd86196fd12dc98394d2fa61c950a
myteam123223 <146390286+myteam123223@users.noreply.github.com>
2025-03-06 00:26:56 +0100

    Update myConfig.ts

--- sendVideo/myConfig.ts
+++ sendVideo/myConfig.ts
@@ -1,3 +1,8 @@
-const N_VIDEOS = 6
+// 1. Actualización de sendVideo/myConfig.ts
+// Este archivo contiene la configuración del bot
 
-export { N_VIDEOS }
+const N_VIDEOS = 6;
+// Nombre del subreddit específico que quieres usar (sin el "r/")
+const SUBREDDIT = "blowjobs";
+
+export { N_VIDEOS, SUBREDDIT };
